0554bc328e9c29b671397197a15e2119a1517d40
soomin <riato101@naver.com>
2023-02-10 10:24:37 +0900

    구글 지도, footer, 파트너십 이미지 박스

--- js/index.js
+++ js/index.js
@@ -1,13 +1,23 @@
+// header
 $(".menu > li").mouseenter(function(){
     $(".submenu").stop().slideDown(400);
 })
 $(".menu> li").mouseleave(function(){
     $(".submenu").stop().slideUp(100);
 })
+// gototop
 $(window).scroll(function(){
 	if ($(this).scrollTop() > 300){
 		$('.gototop').addClass('active')
 	} else{
 		$('.gototop').removeClass('active')
 	}
+});
+// partner tab
+$('.partner-content').hide().eq(0).show();
+$('.partner-title span').click(function(){
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+    $('.partner-content').fadeOut();
+    $('#' + $(this).attr('data-alt')).fadeIn();
 });
