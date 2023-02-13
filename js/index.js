9bfff708185c60d46a13ddceceaae81660a05fca
soomin <riato101@naver.com>
2023-02-13 13:43:41 +0900

    typeit, 서비스 탭메뉴로 바꿈, partner 바꿈

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ $(window).scroll(function(){
 		$('.gototop').removeClass('active')
 	}
 });
-// partner tab
+// partner - tab
 $('.partner-content').hide().eq(0).show();
 $('.partner-title span').click(function(){
     $(this).addClass('active');
@@ -21,3 +21,18 @@ $('.partner-title span').click(function(){
     $('.partner-content').fadeOut();
     $('#' + $(this).attr('data-alt')).fadeIn();
 });
+// service - tab
+$('.service-content').hide().eq(0).show();
+$('.service-title span').click(function(){
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+    $('.service-content').fadeOut();
+    $('#' + $(this).attr('data-alt')).fadeIn();
+});
+// introduce - TypeIt
+new TypeIt("#typing", {
+    strings: "Kephas!",
+    speed: 50,
+    loop: false,
+    waitUntilVisible: true,
+  }).go();
