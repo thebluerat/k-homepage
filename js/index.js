c58607a2af1613862bc5e02ef4128ba0cabee656
soomin <riato101@naver.com>
2023-02-15 01:12:34 +0900

    전체 디자인 수정

---
+++ js/index.js
@@ -0,0 +1,41 @@
+// header
+$(".menu > li").mouseenter(function(){
+    $(".submenu").stop().slideDown(400);
+})
+$(".menu> li").mouseleave(function(){
+    $(".submenu").stop().slideUp(100);
+})
+// gototop
+$(window).scroll(function(){
+	if ($(this).scrollTop() > 300){
+		$('.gototop').addClass('active')
+	} else{
+		$('.gototop').removeClass('active')
+	}
+});
+// partner - tab
+$('.partner-content').hide().eq(0).show();
+$('.partner-title span').click(function(){
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+    $('.partner-content').fadeOut();
+    $('#' + $(this).attr('data-alt')).fadeIn();
+});
+// service - tab
+$('.service-content').hide().eq(0).show();
+$('.service-title a').click(function(){
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+    $('.service-content').fadeOut();
+    $('#' + $(this).attr('data-alt')).fadeIn();
+});
+// introduce - TypeIt
+new TypeIt("#typing", {
+    strings: "Kephas!",
+    speed: 30,
+    loop: false,
+    waitUntilVisible: true,
+    afterComplete: function (instance) {
+        instance.destroy();
+    }
+  }).go();
